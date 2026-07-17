6aabcb9e583504981f973b50f4960c59fde82626
Daniel <justus.daniel@web.de>
2026-07-17 23:01:15 +0200

    Fix symmeet formula asset name to match release pipeline output
    The release pipeline packages the CLI as symmeet_v<VERSION>_darwin_arm64.tar.gz
    (see scripts/package-cli.sh in danieljustus/symaira-meet). The formula URL was
    missing the v prefix in the file name and would 404 after release.
    
    Refs danieljustus/symaira-meet#66

--- Formula/symmeet.rb
+++ Formula/symmeet.rb
@@ -1,7 +1,7 @@
 class Symmeet < Formula
   desc "Local-first, standalone meeting-artifact tool"
   homepage "https://github.com/danieljustus/symaira-meet"
-  url "https://github.com/danieljustus/symaira-meet/releases/download/v0.4.0-beta.1/symmeet_0.4.0-beta.1_darwin_arm64.tar.gz"
+  url "https://github.com/danieljustus/symaira-meet/releases/download/v0.4.0-beta.1/symmeet_v0.4.0-beta.1_darwin_arm64.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "Apache-2.0"
 
