8129e705bcdfe8685c238aa7927ab7df2bd7f365
Daniel <justus.daniel@web.de>
2026-07-20 00:51:31 +0200

    symmeet v0.4.0-beta.1 checksum correction

--- Casks/symmeet-agent.rb
+++ Casks/symmeet-agent.rb
@@ -1,6 +1,6 @@
 cask "symmeet-agent" do
   version "0.4.0-beta.1"
-  sha256 "14662f5f35c58f9fc428dcfbad1b0ed6da870c2869141da00fd7a2f7a5a5cc26"
+  sha256 "94532915fdfe1b9491f892bcb45e229870c57f6c48769d7fefa36c9b86816940"
 
   url "https://github.com/danieljustus/symaira-meet/releases/download/v0.4.0-beta.1/SymMeetAgent_v0.4.0-beta.1.dmg"
   name "SymMeetAgent"
